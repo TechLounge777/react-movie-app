02515444b63e126ec912c365dc12831ad6c4d21d
TechLounge777 <msblanche777@gmail.com>
2021-03-02 20:56:13 +0100

    added useffect and data fetch

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React,  {useState, useEffect} from 'react';
 import './App.css';
+import MovieList from './components/MovieList';
 
-function App() {
+/* Bootstrap CSS */
+import 'bootstrap';
+
+/* jQuery */
+import JQuery from 'jquery';
+window.$ = JQuery;
+
+const App = () => { // eslint-disable-next-line
+  const [movies, setMovies] = useState([]);
+
+  const gestMovieRequest = async () => {
+    const url = "http://www.omdbapi.com/?s=horror&apikey=d6fd7dc7";
+
+    const response = await fetch(url);
+    const responseJson = await response.json();
+
+    console.log(responseJson);
+    setMovies(responseJson.Search);
+  };
+
+  useEffect(() => {
+    gestMovieRequest();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <div className='container-fluid movie-app'>
+    <div className='row'>
+      <MovieList movies={movies} />
     </div>
+  </div>
   );
-}
+};
 
 export default App;
+
